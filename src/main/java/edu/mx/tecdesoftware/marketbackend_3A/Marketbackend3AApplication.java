e2f9dd0f84fb35be8e743b610b4fcbcbc77b1ab2
asus <asus@LAPTOP-H6PM25FQ>
2026-05-18 03:24:06 -0600

    chore: initial Spring Boot project setup

---
+++ src/main/java/edu/mx/tecdesoftware/marketbackend_3A/Marketbackend3AApplication.java
@@ -0,0 +1,13 @@
+package edu.mx.tecdesoftware.marketbackend_3A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Marketbackend3AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Marketbackend3AApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mx/tecdesoftware/marketbackend_3A/Marketbackend3AApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mx.tecdesoftware.marketbackend_3A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Marketbackend3AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
