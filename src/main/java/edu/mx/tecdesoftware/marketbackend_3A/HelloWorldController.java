d11cf570070b92dd76d18171518afb6c9a9d3206
asus <asus@LAPTOP-H6PM25FQ>
2026-05-18 06:45:07 -0600

    test: Adding HelloWorldController to test Spring

--- src/main/java/edu/mx/tecdesoftware/marketbackend_3A/HelloWorldController.java
+++ src/main/java/edu/mx/tecdesoftware/marketbackend_3A/HelloWorldController.java
@@ -1,8 +1,11 @@
 package edu.mx.tecdesoftware.marketbackend_3A;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @GetMapping("/hola")
+    public String saludar() { return "Hello World"; }
 }
